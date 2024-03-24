e277a173a45264b42e6776b741571a44968212d0
mshioyz <mshioyz@gmail.com>
2024-03-25 08:17:47 +0900

    process

--- model/exchange.py
+++ model/exchange.py
@@ -5,6 +5,6 @@ class Exchange:
     def __init__(self, buy_orders=None, sell_orders=None, order_queue=None):
         self.buy_orders = []
         self.sell_orders = []
-        self.order_queue = []
+        self.order_queue = queue.Queue()
 
     
--- util/process_order.py
+++ util/process_order.py
@@ -1,7 +1,13 @@
 from model.order import Order
 
 def process_order(order) -> Order:
-    
-    
-
-    return None
+    orderObj = Order(
+        order_id = order['order_id'],
+        customer_id = order['customer_id'],
+        order_volume = order['order_volume'],
+        ticker = order['ticker'],
+        price = order['price'],
+        time_of_order = order['id'], # possible substitute
+        order_type = order['order_type']
+    )
+    return orderObj
